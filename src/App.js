1b9586a1d8085472ad76da515cfb0ab4cf0fbd45
davidgzdeveloper <davidgzdeveloper@gmail.com>
2022-11-24 22:18:50 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Mensaje from './Mensaje';
 import './App.css';
+
+const Description = () => {             /*los componentes SIEMPRE EMPIEZAN EN MAYUSCULA*/
+ return <p>Esta es la app del fullstack bootcamp</p>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje  color = 'red'  message= 'Estamos trabajando'/>
+      <Mensaje color = 'yellow'message= 'En un curso'/>
+      <Mensaje color = 'green' message= 'de React'/>
+      <Description />
     </div>
   );
 }
